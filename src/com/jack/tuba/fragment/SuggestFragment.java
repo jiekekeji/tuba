f8c5b03e56b5aa55e04eea0082dddbb28552e4f2
jiekekeji <jieke_rose@foxmail.com>
2015-01-16 23:18:20 +0800

    下载

---
+++ src/com/jack/tuba/fragment/SuggestFragment.java
@@ -0,0 +1,18 @@
+package com.jack.tuba.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SuggestFragment extends Fragment{
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+
+}
